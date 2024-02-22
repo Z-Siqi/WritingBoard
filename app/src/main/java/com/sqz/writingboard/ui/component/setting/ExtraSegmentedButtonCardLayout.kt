11c4759fc40625b53c863cb2e8ccaf50caaf3371
Siqi Zhao <77391690+Z-Siqi@users.noreply.github.com>
2024-02-22 18:49:52 +0800

    VersionCode: 0.5.5-beta2 Fixed a strange crash

--- app/src/main/java/com/sqz/writingboard/ui/component/setting/ExtraSegmentedButtonCardLayout.kt
+++ app/src/main/java/com/sqz/writingboard/ui/component/setting/ExtraSegmentedButtonCardLayout.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SegmentedButton
@@ -27,8 +28,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.sqz.writingboard.R
+import com.sqz.writingboard.ui.theme.themeColor
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -123,7 +127,22 @@ fun ExtraButtonCardLayout(
                         onCheckedChange = onCheckedChange
                     )
                 }
-            }
+            } else { Box { /* what the fucking strange bug??? */ } }
         }
+    }
+}
+
+@Preview
+@Composable
+private fun Preview() {
+    val cardColors = CardDefaults.cardColors(containerColor = themeColor("cardColor"))
+    var theme = 0
+    SegmentedButtonCardLayout(
+        title = "test",
+        options = listOf(R.string.app_name, R.string.app_name, R.string.app_name),
+        selectedOption = theme,
+        colors = cardColors
+    ) { index ->
+        theme = index
     }
 }
